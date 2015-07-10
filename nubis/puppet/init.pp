7670cb9562285a846863e4e1bbfcbdd74831cf42
Justin Dow <justin@dowhaus.com>
2015-07-10 12:08:20 -0700

    adding expect and mailutils packages

--- nubis/puppet/init.pp
+++ nubis/puppet/init.pp
@@ -1,2 +1,2 @@
 # empty init.pp file, puppet manifests that you want and need should go in here for your project
-import okta.pp
+import 'okta.pp'
--- nubis/puppet/okta.pp
+++ nubis/puppet/okta.pp
@@ -10,11 +10,17 @@ package {
     'default-jre':
         before  => Package['OktaLDAPAgent-01.02.07_amd64'],
         ensure => latest;
-    'libgl1':
-        before  => Package['OktaLDAPAgent-01.02.07_amd64'],
-        ensure => latest;
     'OktaLDAPAgent-01.02.07_amd64':
         provider => 'dpkg',
         ensure   => installed,
-        source   => '/root/OktaLDAPAgent-01.02.07_amd64.deb',
+        source   => '/root/OktaLDAPAgent-01.02.07_amd64.deb';
+    'expect':
+        ensure => latest;
+    'mailutils':
+        ensure => latest;
+}
+
+host {
+    'ldap.db.scl3.mozilla.com':
+        ip => '10.22.70.17';
 }
