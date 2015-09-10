a323f491905d5a018a2621a15f4192c340385b5d
limed <limed@mozilla.com>
2015-09-10 16:56:37 -0700

    Do an apt-get update

--- nubis/puppet/init.pp
+++ nubis/puppet/init.pp
@@ -1,3 +1,8 @@
-# empty init.pp file, puppet manifests that you want and need should go in here for your project
+
+# Update Apt first
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+}
+
 import 'okta.pp'
 import 'okta_confd.pp'
